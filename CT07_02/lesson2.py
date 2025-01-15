e57aaa7fcfe02de7d07dfcc974cdb40b7ff9da66
ThemeGithub <olliken17@gmail.com>
2025-01-15 07:57:17 +0000

    Jan 15, 2025, 7:57 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -3,7 +3,7 @@ import random
 while True:
     n1 = random.randint(2, 6)
     n2 = random.randint(2, 4)
-    ans = int(input(str(n1) + "^" + str(n2) + " = "))
+    ans = int(input(str(n1) + " ^ " + str(n2) + " = "))
     if ans == n1**n2:
         print("Correct")
         break
