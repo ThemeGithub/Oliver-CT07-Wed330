73faa2041fc499169123996ee6cd0f8317a5507f
ThemeGithub <olliken17@gmail.com>
2025-01-15 07:55:16 +0000

    Jan 15, 2025, 7:55 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -3,7 +3,7 @@ import random
 while True:
     n1 = random.random(2, 6)
     n2 = random.random(2, 4)
-    ans = int(input("1 + 1 "))
+    ans = int(input(str(n1) ))
     if ans == 2:
         print("Correct")
         break
