65d5b138f0f510181c3a13b5a8a59c069403b129
ThemeGithub <olliken17@gmail.com>
2025-01-15 07:54:46 +0000

    Jan 15, 2025, 7:54 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,7 +1,8 @@
 import random
 
 while True:
-    n1 = math.random(2, )
+    n1 = math.random(2, 6)
+    n2 = random.random
     ans = int(input("1 + 1 "))
     if ans == 2:
         print("Correct")
