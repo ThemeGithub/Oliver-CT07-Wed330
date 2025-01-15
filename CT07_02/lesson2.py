fcc8c5c6956f953f384156e2aaa89de1c547082e
ThemeGithub <olliken17@gmail.com>
2025-01-15 07:58:03 +0000

    Jan 15, 2025, 7:58 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,8 +1,8 @@
 import random
 
+n1 = random.randint(2, 6)
+n2 = random.randint(2, 4)
 while True:
-    n1 = random.randint(2, 6)
-    n2 = random.randint(2, 4)
     ans = int(input(str(n1) + " ^ " + str(n2) + " = "))
     if ans == n1**n2:
         print("Correct")
