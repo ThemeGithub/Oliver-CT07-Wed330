a7bef77c5a438e6fb3f0480c626d2473598e3b69
ThemeGithub <olliken17@gmail.com>
2025-01-15 08:25:37 +0000

    Jan 15, 2025, 8:25 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,17 +1,20 @@
-import random
+# import random
 
-score = 0
-for i in range(10):
-    n1 = random.randint(2, 6)
-    n2 = random.randint(2, 4)
-    while True:
-        ans = int(input(f'{n1} ^ {n2} = '))
-        if ans == n1**n2:
-            print("Correct, + 1000")
-            score += 1000
-            break
-        else:
-            print("Wrong, - 1000")
-            score -= 1000
+# score = 0
+# for i in range(10):
+#     n1 = random.randint(2, 6)
+#     n2 = random.randint(2, 4)
+#     while True:
+#         ans = int(input(f'{n1} ^ {n2} = '))
+#         if ans == n1**n2:
+#             print("Correct, + 1000")
+#             score += 1000
+#             break
+#         else:
+#             print("Wrong, - 1000")
+#             score -= 1000
 
-print(score)
+# print(score)
+
+while True:
+    
