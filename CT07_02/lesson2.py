f258b40eca6abc8e7ee68efc993ece689945ccf5
ThemeGithub <olliken17@gmail.com>
2025-01-15 07:59:44 +0000

    Jan 15, 2025, 7:59 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -3,7 +3,7 @@ import random
 n1 = random.randint(2, 6)
 n2 = random.randint(2, 4)
 while True:
-    ans = int(input(f''))
+    ans = int(input(f'{}'))
     if ans == n1**n2:
         print("Correct")
         break
