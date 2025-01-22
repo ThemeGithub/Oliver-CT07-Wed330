eec79acb224045f48e7dab3a92a64034bbc9533e
ThemeGithub <olliken17@gmail.com>
2025-01-22 07:42:39 +0000

    Jan 22, 2025, 7:42 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -1,6 +1,8 @@
 answer = ""
-while answer != "answer":
+for i in range(3):
     answer = input("type answer to answer this riddle: ")
     if not answer == "answer":
         print("read the question")
+    else:
+        break
 print("Correct")
