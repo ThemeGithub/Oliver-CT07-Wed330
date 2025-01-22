07e0713b6b662c5d7f8715136e81b6f92c494323
ThemeGithub <olliken17@gmail.com>
2025-01-22 07:57:47 +0000

    Jan 22, 2025, 7:57 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -17,7 +17,7 @@ import time
 timer = int(input("How many minutes is the timer? "))
 ttime = timer
 for i in range(timer):
-    time.sleep(60)
+    time.sleep(10)
     ttime =- 1
     ans = input("You have " + str(ttime) + " minutes left") == "stop"
     if ans == "stop":
