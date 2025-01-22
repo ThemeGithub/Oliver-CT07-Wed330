232ece6be2d726cd8bc087e02c86d77b3fe69eeb
ThemeGithub <olliken17@gmail.com>
2025-01-22 07:53:40 +0000

    Jan 22, 2025, 7:53 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -1,15 +1,17 @@
-import random
-hints = ["read the question", "it starts with 'A'", "it ends with 'R'", "it has 2 voewls", "it is in the question"]
-answer = ""
-for i in range(3):
-    answer = input("type answer to answer this riddle: ")
-    if not answer == "answer":
-        hint = random.randint(1, len(hints))
-        print(hints[hint])
-        hints.pop(hint)
-    else:
-        break
-if answer == "answer":
-    print("Correct")
-else:
-    print("u are dumb")
+# import random
+# hints = ["read the question", "it starts with 'A'", "it ends with 'R'", "it has 2 voewls", "it is in the question"]
+# answer = ""
+# for i in range(3):
+#     answer = input("type answer to answer this riddle: ")
+#     if not answer == "answer":
+#         hint = random.randint(1, len(hints))
+#         print(hints[hint])
+#         hints.pop(hint)
+#     else:
+#         break
+# if answer == "answer":
+#     print("Correct")
+# else:
+#     print("u are dumb")
+
+in
