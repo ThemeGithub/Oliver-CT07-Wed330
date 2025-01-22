c7ecf6b7418d761456cfd7646630d11062292717
ThemeGithub <olliken17@gmail.com>
2025-01-22 08:18:08 +0000

    Jan 22, 2025, 8:18 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -14,31 +14,34 @@
 # else:
 #     print("u are dumb")
 
-import time
-timer = int(input("How many minutes is the timer? "))
-print("Addons:")
-print("CountUp: Counts up, instead of down")
-print("Hours: Takes in hours instead of minutes")
-print("Seconds: Takes in seconds instead of minutes")
-print("Stopwatch: Tells you how much time there is every minute, no timer")
-addon = input("Which addons do you want to implement? ")
-if addon == "Stopwatch":
-    addon = "CountUp"
-    timer = 99999999**99999999**9999999999**9999999999**999999999**9999999**999999999
-timeunit = "minutes"
-ttime = timer
-for i in range(timer):
-    if addon == "Hours":
-        time.sleep(3600)
-        timeunit = "hours"
-    elif addon == "Seconds":
-        time.sleep(1)
-        timeunit = "seconds"
-    else:
-        time.sleep(60)
-    ttime = ttime - 1
-    if addon == "CountUp":
-        print("You have " + str(timer - ttime) + " " + timeunit + " left")
-    else:
-        print("You have " + str(ttime) + " " + timeunit + " left")
-print("TIMES UP")
+# import time
+# timer = int(input("How many minutes is the timer? "))
+# print("Addons:")
+# print("CountUp: Counts up, instead of down")
+# print("Hours: Takes in hours instead of minutes")
+# print("Seconds: Takes in seconds instead of minutes")
+# print("Stopwatch: Tells you how much time there is every minute, no timer")
+# addon = input("Which addons do you want to implement? ")
+# if addon == "Stopwatch":
+#     addon = "CountUp"
+#     timer = 99999999**99999999**9999999999**9999999999**999999999**9999999**999999999
+# timeunit = "minutes"
+# ttime = timer
+# for i in range(timer):
+#     if addon == "Hours":
+#         time.sleep(3600)
+#         timeunit = "hours"
+#     elif addon == "Seconds":
+#         time.sleep(1)
+#         timeunit = "seconds"
+#     else:
+#         time.sleep(60)
+#     ttime = ttime - 1
+#     if addon == "CountUp":
+#         print("You have " + str(timer - ttime) + " " + timeunit + " left")
+#     else:
+#         print("You have " + str(ttime) + " " + timeunit + " left")
+# print("TIMES UP")
+money = 0
+while True:
+    add = ""
