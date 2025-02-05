b36bf80f6ad90a17cd1bfdde19138afcacbc060d
ThemeGithub <olliken17@gmail.com>
2025-02-05 07:43:51 +0000

    Feb 5, 2025, 7:43 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -4,8 +4,8 @@ left = 10
 while left != 0:
     t.sleep(1)
     t = t - 1
-    if r.randint(1, 10) == 1:
+    if r.randint(1, 1) == 1:
         print("AAAAAAH")
         break
 else:
-    
+    print("NEW YEAR HAPPY")
