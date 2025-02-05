086e43eb90a1014c8f657537a71e5c92a96d5f72
ThemeGithub <olliken17@gmail.com>
2025-02-05 08:30:13 +0000

    Feb 5, 2025, 8:30 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -11,17 +11,18 @@
 # else:
 #     print("NEW YEAR HAPPY")
 
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
-planets[3] = "J1407b"
-planets.append("Plucto")
-planets.insert(3, "Land")
-planets.insert(6, planets.pop(5))
-for i in planets:
-    if i == "Earth":
-        print("Eart: This is not not not not not not my planet")
-    elif i == "J1407b":
-        print("b7041J: COnquerer: me, yay")
-    elif i == "Land":
-        print("Land: made in china (me)")
-    else:
-        print(i)
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+# planets[3] = "J1407b"
+# planets.append("Plucto")
+# planets.insert(3, "Land")
+# planets.insert(6, planets.pop(5))
+# for i in planets:
+#     if i == "Earth":
+#         print("Eart: This is not not not not not not my planet")
+#     elif i == "J1407b":
+#         print("b7041J: COnquerer: me, yay")
+#     elif i == "Land":
+#         print("Land: made in china (me)")
+#     else:
+#         print(i)
+
