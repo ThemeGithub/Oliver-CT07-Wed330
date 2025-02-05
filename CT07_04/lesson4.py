dbf35ea3dc75d86dd9073c9d6ef7a878a9eb776a
ThemeGithub <olliken17@gmail.com>
2025-02-05 07:51:32 +0000

    Feb 5, 2025, 7:51 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,12 +1,12 @@
-import time as t
-import random as r
-left = 10
-while left != 0:
-    print(left)
-    t.sleep(1)
-    left = left - 1
-    if r.randint(1, 20) == 1:
-        print("AAAAAAH")
-        break
-else:
-    print("NEW YEAR HAPPY")
+# import time as t
+# import random as r
+# left = 10
+# while left != 0:
+#     print(left)
+#     t.sleep(1)
+#     left = left - 1
+#     if r.randint(1, 20) == 1:
+#         print("AAAAAAH")
+#         break
+# else:
+#     print("NEW YEAR HAPPY")
