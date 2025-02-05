2230ad864452b975031bb8bdd94e54d16a52327c
ThemeGithub <olliken17@gmail.com>
2025-02-05 08:23:08 +0000

    Feb 5, 2025, 8:23 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -17,11 +17,11 @@ planets.append("Plucto")
 planets.insert(3, "Land")
 planets.insert(6, planets.pop(5))
 for i in planets:
-    print(i)
     if i == "Earth":
         print("This is not not not not not not my planet")
     elif i == "J1407b":
         print("COnquerer: me, yay")
     elif i == "Land":
         print("made in china (me)")
-    
+    else:
+        print(i)
