a88ac2f64c43a67b0cbb4cd0f15d0c8c54437982
ThemeGithub <olliken17@gmail.com>
2025-02-12 08:17:38 +0000

    Feb 12, 2025, 8:17 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -41,4 +41,4 @@ namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
             ]
 heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 
-print(namelist[heightlist.max(heightlist)])
+print(namelist[heightlist.index(max(heightlist))])
