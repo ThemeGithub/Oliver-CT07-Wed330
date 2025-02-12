f961a7bb8c3b1ff90d96d5ba5bc3a0ac956b0020
ThemeGithub <olliken17@gmail.com>
2025-02-12 08:26:30 +0000

    Feb 12, 2025, 8:26 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -47,3 +47,20 @@
 #     del(heightlist[heightlist.index(max(heightlist))])
 #     print(namelist[heightlist.index(max(heightlist))])
 # print(namelist[heightlist.index(min(heightlist))])
+
+pokemons = [
+    "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+    "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+    "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+    "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+    "Electabuzz"
+]
+
+powers = [
+    55, 84, 49, 48, 45,
+    45, 52, 55, 110, 110,
+    85, 65, 134, 130, 110,
+    50, 125, 65, 110, 83
+]
+
+
