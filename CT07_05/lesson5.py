132ec6151ba2fd43356d1ceff3a8e863cc9ef900
ThemeGithub <olliken17@gmail.com>
2025-02-12 08:16:17 +0000

    Feb 12, 2025, 8:16 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -40,3 +40,5 @@ namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
             "Sophia", "Lucas", "Mia", "Aiden"
             ]
 heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+
+print()
