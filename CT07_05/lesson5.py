b5348b70381d8c32ae56662f4a3dc0a8e6811022
ThemeGithub <olliken17@gmail.com>
2025-02-12 08:16:05 +0000

    Feb 12, 2025, 8:16 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -16,21 +16,27 @@
 # for i in nums:
 #     print(i)
 # print(int(input("choose num ")) in nums)
-import random as r
-import math as m
-scores = []
-for i in range(100):
-    scores.append(r.randint(0, 100))
+# import random as r
+# import math as m
+# scores = []
+# for i in range(100):
+#     scores.append(r.randint(0, 100))
 
-print(min(scores))
-print(max(scores))
-total = 0
-for i in range(100):
-    total = total + scores[i]
-average = total / 100
-print(average)
-squaretotal = 0
-for i in range(100):
-    squaretotal = (average - scores[i]) ** 2
-avsquare = squaretotal / 100
-print(m.sqrt(avsquare))
+# print(min(scores))
+# print(max(scores))
+# total = 0
+# for i in range(100):
+#     total = total + scores[i]
+# average = total / 100
+# print(average)
+# squaretotal = 0
+# for i in range(100):
+#     squaretotal = (average - scores[i]) ** 2
+# avsquare = squaretotal / 100
+# print(m.sqrt(avsquare))
+
+
+namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
+            "Sophia", "Lucas", "Mia", "Aiden"
+            ]
+heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
