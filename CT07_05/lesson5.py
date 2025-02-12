64edd9df57df44c4412952581a156f0e87c5fb32
ThemeGithub <olliken17@gmail.com>
2025-02-12 07:41:26 +0000

    Feb 12, 2025, 7:41 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,6 +1,7 @@
-food = ["air", "oxygen", "water", "lunch", "dinner"]
-del(food[2])
-# food.pop(2)
-food.append("breakfast")
-for i in food:
-    print(i)
+# food = ["air", "oxygen", "water", "lunch", "dinner"]
+# del(food[2])
+# # food.pop(2)
+# food.append("breakfast")
+# for i in food:
+#     print(i)
+
