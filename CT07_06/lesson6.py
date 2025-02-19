b96a2e4b1eb2a114f064752528bd67eeec4c5877
ThemeGithub <olliken17@gmail.com>
2025-02-19 08:04:12 +0000

    Feb 19, 2025, 8:04 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,15 +1,15 @@
 import random as r
 import time as t
-selected = []
-for i in range(100):
-    num = r.randint(1, 1000)
-    while num in selected:
-        num = r.randint(1, 100)
-    selected.append(num)
-    print(num)
-print(min(selected))
-print(max(selected))
-print(sum(selected) / len(selected))
-ran = r.choice(selected)
-print(ran)
-print(selected.index(ran))
+# selected = []
+# for i in range(100):
+#     num = r.randint(1, 1000)
+#     while num in selected:
+#         num = r.randint(1, 100)
+#     selected.append(num)
+#     print(num)
+# print(min(selected))
+# print(max(selected))
+# print(sum(selected) / len(selected))
+# ran = r.choice(selected)
+# print(ran)
+# print(selected.index(ran))
