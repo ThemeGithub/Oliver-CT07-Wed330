ddde2df506af39e6cc791e928c8531f71d410f90
ThemeGithub <olliken17@gmail.com>
2025-02-19 07:38:21 +0000

    Feb 19, 2025, 7:38 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -3,7 +3,7 @@ import time as t
 selected = []
 for i in range(100):
     num = r.randint(1, 1000)
-    while num not in selected:
+    while num in selected:
         num = r.randint(1, 100)
     selected.append(num)
     print(num)
