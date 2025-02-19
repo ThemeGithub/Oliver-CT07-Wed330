194a69b301ec5023b63cba2f3a864028e0072c60
ThemeGithub <olliken17@gmail.com>
2025-02-19 08:15:02 +0000

    Feb 19, 2025, 8:15 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -14,13 +14,21 @@ import time as t
 # print(ran)
 # print(selected.index(ran))
 
-contacts = []
-contact1 = ["John", 98453126, "john@gmail.com"]
-contact2 = ["Adam", 93029102, "adam@gmail.com"]
-contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
-contacts.append(contact1)
-contacts.append(contact2)
-contacts.append(contact3)
-for i in contacts:
-    for j in i:
-        print(j)
+# contacts = []
+# contact1 = ["John", 98453126, "john@gmail.com"]
+# contact2 = ["Adam", 93029102, "adam@gmail.com"]
+# contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+# contacts.append(contact1)
+# contacts.append(contact2)
+# contacts.append(contact3)
+# for i in contacts:
+#     for j in i:
+#         print(j)
+
+students = [
+    ["Olivia", "F"], ["Noah", "M"], ["Emma", "F"],
+    ["Liam", "M"], ["Ava", "F"], ["Ethan", "M"],
+    ["Sophia", "F"], ["Lucas", "M"], ["Mia", "F"],
+    ["Aiden", "M"], ["Isabella", "F"], ["Jackson", "M"],
+    ["Amelia", "F"], ["Logan", "M"], ["Lily", "F"]
+]
