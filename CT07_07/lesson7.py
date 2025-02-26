003b89ed1c26f51fa9a7213f0372326fe8c5f14d
ThemeGithub <olliken17@gmail.com>
2025-02-26 07:47:22 +0000

    Feb 26, 2025, 7:47 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,6 +1,6 @@
 people = [
-    ["name", 12345678, "Co-"]
-    ["student", 8765432, "Curriculum"]
+    ["name", 12345678, "Co-"],
+    ["student", 8765432, "Curriculum"],
     ["person", 91827364, "Activities"]
 ]
 
