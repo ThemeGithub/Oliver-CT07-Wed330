1274f15235ac39020d9a9cec5652a19d62101ecf
ThemeGithub <olliken17@gmail.com>
2025-02-26 07:55:50 +0000

    Feb 26, 2025, 7:55 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,10 +1,14 @@
-name = ["name", 12345678, "Co-"]
-student = ["student", 87654321, "Curriculum"]
-person = ["person", 91827364, "Activities"]
+# name = ["name", 12345678, "Co-"]
+# student = ["student", 87654321, "Curriculum"]
+# person = ["person", 91827364, "Activities"]
 
-people = []
-people.append(name)
-people.append(student)
-people.append(person)
-for i in people:
-    print("name: " + i[0] + " number: " + str(i[1]) + " cca: " + i[2])
+# people = []
+# people.append(name)
+# people.append(student)
+# people.append(person)
+# for i in people:
+#     print("name: " + i[0] + " number: " + str(i[1]) + " cca: " + i[2])
+
+list1 = ["Apple", "Banana", "Cherry"]
+list2 = ["Durian", "Elderberry", "Figs"]
+
