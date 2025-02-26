03818206de487173eaf87d389e019a697d6bdd5d
ThemeGithub <olliken17@gmail.com>
2025-02-26 07:51:07 +0000

    Feb 26, 2025, 7:51 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -2,12 +2,7 @@ name = ["name", 12345678, "Co-"]
 student = ["student", 87654321, "Curriculum"]
 person = ["person", 91827364, "Activities"]
 
-
-people = [
-    ["name", 12345678, "Co-"],
-    ["student", 8765432, "Curriculum"],
-    ["person", 91827364, "Activities"]
-]
+people = []
 
 for i in people:
     print("name: " + i[0] + " number: " + str(i[1]) + " cca: " + i[2])
