9e24841330cd9f23f89476f742d89ea2a7e1bbbe
ThemeGithub <olliken17@gmail.com>
2025-03-09 03:30:45 +0000

    Mar 9, 2025, 3:30 AM

--- CT07_Mid_Sem/test.py
+++ CT07_Mid_Sem/test.py
@@ -1,6 +1,9 @@
 import random as r
 print("Hero starts on his adventure with Health: 100")
 health = 100
+battles = 0
 while health > 0:
+    battles += 1
     health = health - r.randint(1, 15)
     print("After fighting monsters, his Health is now: " + str(health))
+print("He fought " + str(battles) + " battles, and died.")
