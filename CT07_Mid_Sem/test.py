e2de7a7750dbbc9aea3b1e0a25809c1cc3aeba06
ThemeGithub <olliken17@gmail.com>
2025-03-09 03:29:48 +0000

    Mar 9, 2025, 3:29 AM

--- CT07_Mid_Sem/test.py
+++ CT07_Mid_Sem/test.py
@@ -3,4 +3,4 @@ print("Hero starts on his adventure with Health: 100")
 health = 100
 while health > 0:
     health = health - r.randint(1, 15)
-    print("After fighting monsters, his Health is now: ")
+    print("After fighting monsters, his Health is now: " + str(health))
