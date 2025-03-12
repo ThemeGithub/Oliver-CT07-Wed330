7cc64c0301133c25081b29a2faf95cc90360ce94
ThemeGithub <olliken17@gmail.com>
2025-03-12 08:18:10 +0000

    Mar 12, 2025, 8:18 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -2,3 +2,6 @@ while True:
     pw = input("Enter password: ")
     if pw.isupper:
         print("You must have lowercase")
+    elif pw.islower:
+        print("You must have uppercase")
+    elif pw.is
