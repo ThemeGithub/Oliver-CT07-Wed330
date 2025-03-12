4e418fa9d9a2b196e796eccd255e62d5f8ae3c58
ThemeGithub <olliken17@gmail.com>
2025-03-12 08:16:33 +0000

    Mar 12, 2025, 8:16 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -2,4 +2,5 @@ while True:
     pw = input("Enter password: ")
     if not pw.isupper and not pw.islower and pw.isalnum and not pw.isalpha and not pw.isnumeric:
         break
-    else
+    else:
+        print("Your password must have uppercase, lowercase, alpha")
