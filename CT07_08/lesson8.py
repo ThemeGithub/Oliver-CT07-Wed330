c0f6ed9fa33eb9ad8b6a0a8f2c6ef9ad8346dfa2
ThemeGithub <olliken17@gmail.com>
2025-03-12 07:52:11 +0000

    Mar 12, 2025, 7:52 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -2,7 +2,7 @@ list1 = [3, 2, 1]
 list2 = [6, 5, 5]
 list3 = [9, 8, 7]
 list = []
-list.append(list1)
-list.append(list2)
-list.append(list3)
-for
+for i in [list1, list2, list3]:
+    for j in i:
+        if j not in list:
+            list.append(j)
