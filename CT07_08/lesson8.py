1a95cd5b29bd92fdf1fedf7063ec593055e76d53
ThemeGithub <olliken17@gmail.com>
2025-03-12 08:45:58 +0000

    Mar 12, 2025, 8:45 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,17 +1,19 @@
-while True:
-    pw = input("Enter password: ")
-    if len(pw) < 8:
-        print("It must be 8 characters or longer")
-    elif pw.isupper():
-        print("You must have lowercase")
-    elif pw.islower():
-        print("You must have uppercase")
-    elif pw.isdigit():
-        print("You must have alphabets")
-    elif pw.isalpha():
-        print("You must have numbers")
-    elif not pw.isalnum():
-        print("Only alphanumeric characters")
-    else:
-         break
-print("yay")
+# while True:
+#     pw = input("Enter password: ")
+#     if len(pw) < 8:
+#         print("It must be 8 characters or longer")
+#     elif pw.isupper():
+#         print("You must have lowercase")
+#     elif pw.islower():
+#         print("You must have uppercase")
+#     elif pw.isdigit():
+#         print("You must have alphabets")
+#     elif pw.isalpha():
+#         print("You must have numbers")
+#     elif not pw.isalnum():
+#         print("Only alphanumeric characters")
+#     else:
+#         break
+# print("yay")
+
+str
