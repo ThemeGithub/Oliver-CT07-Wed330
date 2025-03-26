9f4f2d8fc8f7a62e337e50012b517d74e6643543
ThemeGithub <olliken17@gmail.com>
2025-03-26 08:02:53 +0000

    Mar 26, 2025, 8:02 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,23 +1,16 @@
-# import turtle
-# window = turtle.Screen()
-# window.setup(width = 600, height = 400)
+import turtle
+window = turtle.Screen()
+window.setup(width = 600, height = 400)
 
-# t = turtle.Turtle()
-# t.shape("turtle")
-# t.fillcolor("orange")
-# for i in range(5):
-#     t.forward(100)
-#     t.left(72)
-
-
+t = turtle.Turtle()
+t.shape("turtle")
+t.fillcolor("orange")
+for i in range(5):
+    t.forward(100)
+    t.left(72)
 
 
-# window.mainloop()
 
 
-def alert():
-    print("Motion Undetected")
-    
-alert()
-alert()
-alert()
+window.mainloop()
+
