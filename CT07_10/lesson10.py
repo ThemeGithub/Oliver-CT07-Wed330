85ac481348fe98cc0ac3bd4311c9755dc52fd86c
ThemeGithub <olliken17@gmail.com>
2025-03-26 08:09:49 +0000

    Mar 26, 2025, 8:09 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,18 +1,19 @@
-import turtle
-window = turtle.Screen()
-window.setup(width = 600, height = 400)
+# import turtle
+# window = turtle.Screen()
+# window.setup(width = 600, height = 400)
 
-t = turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
 
-def square():
-    for i in range(4):
-        t.forward(100)
-        t.left(90)
+# def square():
+#     for i in range(4):
+#         t.forward(100)
+#         t.left(90)
 
 
-square()
+# square()
+
 
 
 
