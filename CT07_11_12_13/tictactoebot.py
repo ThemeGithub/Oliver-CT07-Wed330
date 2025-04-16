2ba64f506e75ecfcb2ac2822007749bf81627474
ThemeGithub <olliken17@gmail.com>
2025-04-16 08:40:31 +0000

    Apr 16, 2025, 8:40 AM

--- CT07_11_12_13/tictactoebot.py
+++ CT07_11_12_13/tictactoebot.py
@@ -111,10 +111,10 @@ def aimove(board, cplayer):
     if board[1][1] == " ":
         board[1][1] = cplayer
         return board
-
-
-
-
+    empty = []
+    for i in [board[0][0], board[0][2], board[2][0], board[2][2]]:
+        if i == " ":
+            empty.append
 
 
 
