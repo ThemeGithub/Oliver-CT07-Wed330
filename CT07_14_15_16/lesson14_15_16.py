3fc2fc5e1169b6fcaad932f18f8543d06e9f504e
ThemeGithub <olliken17@gmail.com>
2025-04-27 03:33:26 +0000

    Apr 27, 2025, 3:33 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -1,7 +1,11 @@
 import pygame
+
 pygame.init()
 width = 800
 height = 600
 screen = pygame.display.set_mode((width, height))
 screen = pygame.display.set_caption("Pong Game")
 
+running = True
+while running:
+    pass
