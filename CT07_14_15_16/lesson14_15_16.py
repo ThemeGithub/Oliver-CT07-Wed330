221cd42ea995a9d87006297feea484391fe52e22
ThemeGithub <olliken17@gmail.com>
2025-04-27 04:02:53 +0000

    Apr 27, 2025, 4:02 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -20,7 +20,7 @@ while running:
     pygame.draw.rect(screen, (0, 100, 0), (10, y1 - paddleHeight / 2, paddleWidth, paddleHeight))
     pygame.draw.rect(screen, (100, 0, 0), (width - paddleWidth - 10, y1 - paddleHeight / 2, paddleWidth, paddleHeight))
     
-    keys = pygame.
+    keys = pygame.key.get_pressed()
 
 
     pygame.display.flip()
