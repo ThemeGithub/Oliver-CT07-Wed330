ba52e1856d0b639c03389c5cb6572465cd96f367
ThemeGithub <olliken17@gmail.com>
2025-04-27 03:49:31 +0000

    Apr 27, 2025, 3:49 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -12,7 +12,7 @@ paddleWidth = 20
 
 running = True
 while running:
-    pygame.draw.rect(screen, "green", (10, y1 - paddleHeight / 2, paddle))
+    pygame.draw.rect(screen, "green", (10, y1 - paddleHeight / 2, paddleWidth, paddleHeight))
 
 
 
