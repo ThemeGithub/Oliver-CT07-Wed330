74b2b75d40fbd56b077b08a4867a0797d6c7d219
ThemeGithub <olliken17@gmail.com>
2025-04-27 04:23:46 +0000

    Apr 27, 2025, 4:23 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -50,7 +50,7 @@ while running:
     
     if ballx < paddle1.right + ballsize and bally > paddle1.top and bally < paddle1.bottom:
         dx = dx * -1
-    if ballx < paddle2.left + ballsize and bally > paddle2.top and bally < paddle1.bottom:
+    if ballx < paddle2.left + ballsize and bally > paddle2.top and bally < paddle2.bottom:
         dx = dx * -1
 
     screen.fill((0, 0, 0))
