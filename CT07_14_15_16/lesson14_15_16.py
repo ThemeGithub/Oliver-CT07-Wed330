279cbc146a21b6ba43f1a84de463ae1c5697a022
ThemeGithub <olliken17@gmail.com>
2025-04-27 03:22:33 +0000

    Apr 27, 2025, 3:22 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -2,5 +2,5 @@ import pygame
 pygame.init()
 width = 800
 height = 600
-screen = pygame.display.setMode(width, height)
-screen = pygame.display.setC
+screen = pygame.display.set.mode(width, height)
+screen = pygame.display.set.caption("Pong Game")
