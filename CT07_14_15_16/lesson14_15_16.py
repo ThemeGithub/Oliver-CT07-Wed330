2ec25a0fc17259f6e6858d21922c5b04b393a8ee
ThemeGithub <olliken17@gmail.com>
2025-04-27 03:50:47 +0000

    Apr 27, 2025, 3:50 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -12,10 +12,6 @@ paddleWidth = 20
 
 running = True
 while running:
-    pygame.draw.rect(screen, "green", (10, y1 - paddleHeight / 2, paddleWidth, paddleHeight))
-
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
