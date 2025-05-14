31f624b29dbf04a5c6ee6862d62d1079b4b1ec42
ThemeGithub <olliken17@gmail.com>
2025-05-14 07:44:41 +0000

    May 14, 2025, 7:44 AM

---
+++ games/blackjack.py
@@ -0,0 +1,29 @@
+'''
+Create the game of Black Jack
+'''
+import random
+import time
+
+suits = ["♣ CLUB", "♦ DIAMOND","❤ HEART","♠ SPADE"]
+ranks = ['2','3','4','5','6','7','8','9','JACK','QUEEN','KING','ACE']
+
+# Step 1: Create the deck of cards
+# Create a dictionary to store the value of each card rank
+# Cards 2–10 are worth their face value, J, Q, K are worth 10, 
+# and Ace can be 1 or 11
+values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+'JACK':10, 'QUEEN':10,'KING':10, 'ACE':11}
+
+# deck = [(rank, suit) for rank in ranks for suit in suits]
+deck = []
+
+# Combine ranks and suits to build a deck of cards
+for suit in suits:
+    for rank in ranks:
+        deck.append([rank, suit])
+
+# print(deck) 
+
+# Step 2: Shuffle the deck
+for i in range(10):
+    random.shuffle(deck) # shuffle this 10 times
