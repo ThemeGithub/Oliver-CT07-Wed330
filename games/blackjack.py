8b0a559323bd2fcbec806799399e7d64ae18c25c
ThemeGithub <olliken17@gmail.com>
2025-05-14 08:00:39 +0000

    May 14, 2025, 8:00 AM

--- games/blackjack.py
+++ games/blackjack.py
@@ -4,7 +4,7 @@ Create the game of Black Jack
 import random
 import time
 
-suits = ["♣", "♦","❤","♠"]
+suits = ["♣", "♦","♡","♠"]
 ranks = ['2','3','4','5','6','7','8','9','JACK','QUEEN','KING','ACE']
 
 # Step 1: Create the deck of cards
