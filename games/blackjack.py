8596e991715dc36fc98e2e8767ede76592920536
ThemeGithub <olliken17@gmail.com>
2025-05-14 08:31:23 +0000

    May 14, 2025, 8:31 AM

--- games/blackjack.py
+++ games/blackjack.py
@@ -100,7 +100,9 @@ def bankermove(hand):
                 if values[card[0]] > left:
                     fail += 1
             if fail / len(deck) > 0.5 + (diff - 6) * 0.05:
-                
+                banker.append(deck.pop())
+            else:
+                end = 1
 
             
         
